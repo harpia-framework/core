f7f7b4967b5fe792df5cad69f1284560e28fdbdc
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2024-12-28 11:43:47 -0300

    fix: inverts the content of the request and response classes
    In a previous commit I made a mistake and ended up inverting the content of request and response.
    This commit solves the problem.

--- src/request.ts
+++ src/request.ts
@@ -1,91 +1,61 @@
 import { Cookies } from "./cookies";
-import { Application } from "./server";
-import type { CookiesOptions } from "./types/cookies";
 
-class ResponseWrapper {
-	private headersInstance: Headers;
-	private statusCode: number;
-	private body: any;
+class RequestWrapper extends Request {
+	public params: Record<string, string>;
+	public query: Record<string, string>;
+
 	private cookiesInstance: Cookies;
-
-	public headers: Headers;
 	public cookies = {
-		set: this.setCookie.bind(this),
+		get: this.getCookie.bind(this),
+		getAll: this.getAllCookies.bind(this),
 	};
 
-	constructor() {
-		this.headersInstance = new Headers();
-		this.statusCode = 200;
-		this.body = null;
-		this.headers = this.getHeadersInstance();
+	constructor(reqInfo: RequestInfo, reqInit: RequestInit, url: string, routePath: string) {
+		super(reqInfo, reqInit);
 
-		this.cookiesInstance = new Cookies();
+		this.params = this.extractParams(url, routePath);
+		this.query = this.extractQuery(url);
+		this.cookiesInstance = new Cookies(this.headers.get("Cookie") ?? undefined);
 	}
 
-	public parse(): Response {
-		this.setCookiesInHeaders();
+	private extractParams(url: string, routePath: string): Record<string, string> {
+		const params: Record<string, string> = {};
 
-		return new Response(this.body, {
-			headers: this.headersInstance,
-			status: this.statusCode,
-		});
-	}
+		const urlPath = new URL(url).pathname;
+		const urlSegments = urlPath.split("/").filter(Boolean);
+		const routeSegments = routePath.split("/").filter(Boolean);
 
-	private getHeadersInstance(): Headers {
-		return this.headersInstance;
-	}
-
-	public status(value: number): this {
-		this.statusCode = value;
-		return this;
-	}
-
-	public send(data: any): this {
-		this.body = data;
-		this.headersInstance.set("Content-Lenght", data.length.toString());
-		return this;
-	}
-
-	public json(data: any): this {
-		this.headersInstance.set("Content-Type", "application/json");
-		this.body = JSON.stringify(data);
-		return this;
-	}
-
-	public async render(view: string, data: Record<string, any> = {}): Promise<this> {
-		const engine = Application.getInstance().engine.get();
-
-		if (!engine) {
-			throw new Error("No template engine configured. Use framework().engine.get()");
+		for (const [index, segment] of routeSegments.entries()) {
+			if (segment.startsWith(":")) {
+				const paramName = segment.slice(1);
+				params[paramName] = urlSegments[index] || "";
+			}
 		}
 
-		try {
-			const rendered = await engine.render(view, data);
-
-			this.headers.set("Content-Type", "text/html");
-			this.body = rendered;
-		} catch (error: any) {
-			throw new Error(`Error rendering template: ${error.message}`);
-		}
-
-		return this;
+		return params;
 	}
 
-	private setCookie(name: string, value: string, options?: CookiesOptions): this {
-		const cookie = this.cookiesInstance.set(name, value, options);
+	private extractQuery(url: string): Record<string, string> {
+		const parsedUrl = new URL(url);
+		const qs = new URL(url, parsedUrl.host).search;
+		const query = new URLSearchParams(qs);
+		const mapper: Record<string, string> = {};
 
-		this.headersInstance.append("Set-Cookie", cookie);
-		return this;
+		for (const [key, value] of query.entries()) {
+			mapper[key] = value;
+		}
+
+		return mapper;
 	}
 
-	private setCookiesInHeaders(): void {
-		const allCookies = this.cookiesInstance.getAll();
+	private getCookie(name: string): string | undefined {
+		return this.cookiesInstance.get(name);
+	}
 
-		for (const [name, value] of Object.entries(allCookies)) {
-			this.headersInstance.append("Set-Cookie", this.cookiesInstance.set(name, value));
-		}
+	private getAllCookies(): Record<string, string> {
+		return this.cookiesInstance.getAll();
 	}
 }
 
-export type FetchResponse = Response;
-export { ResponseWrapper as Response };
+export type FetchRequest = Request;
+export { RequestWrapper as Request };
--- src/response.ts
+++ src/response.ts
@@ -1,61 +1,91 @@
 import { Cookies } from "./cookies";
+import { Application } from "./server";
+import type { CookiesOptions } from "./types/cookies";
 
-class RequestWrapper extends Request {
-	public params: Record<string, string>;
-	public query: Record<string, string>;
-
+class ResponseWrapper {
+	private headersInstance: Headers;
+	private statusCode: number;
+	private body: any;
 	private cookiesInstance: Cookies;
+
+	public headers: Headers;
 	public cookies = {
-		get: this.getCookie.bind(this),
-		getAll: this.getAllCookies.bind(this),
+		set: this.setCookie.bind(this),
 	};
 
-	constructor(reqInfo: RequestInfo, reqInit: RequestInit, url: string, routePath: string) {
-		super(reqInfo, reqInit);
+	constructor() {
+		this.headersInstance = new Headers();
+		this.statusCode = 200;
+		this.body = null;
+		this.headers = this.getHeadersInstance();
 
-		this.params = this.extractParams(url, routePath);
-		this.query = this.extractQuery(url);
-		this.cookiesInstance = new Cookies(this.headers.get("Cookie") ?? undefined);
+		this.cookiesInstance = new Cookies();
 	}
 
-	private extractParams(url: string, routePath: string): Record<string, string> {
-		const params: Record<string, string> = {};
+	public parse(): Response {
+		this.setCookiesInHeaders();
 
-		const urlPath = new URL(url).pathname;
-		const urlSegments = urlPath.split("/").filter(Boolean);
-		const routeSegments = routePath.split("/").filter(Boolean);
+		return new Response(this.body, {
+			headers: this.headersInstance,
+			status: this.statusCode,
+		});
+	}
 
-		for (const [index, segment] of routeSegments.entries()) {
-			if (segment.startsWith(":")) {
-				const paramName = segment.slice(1);
-				params[paramName] = urlSegments[index] || "";
-			}
+	private getHeadersInstance(): Headers {
+		return this.headersInstance;
+	}
+
+	public status(value: number): this {
+		this.statusCode = value;
+		return this;
+	}
+
+	public send(data: any): this {
+		this.body = data;
+		this.headersInstance.set("Content-Lenght", data.length.toString());
+		return this;
+	}
+
+	public json(data: any): this {
+		this.headersInstance.set("Content-Type", "application/json");
+		this.body = JSON.stringify(data);
+		return this;
+	}
+
+	public async render(view: string, data: Record<string, any> = {}): Promise<this> {
+		const engine = Application.getInstance().engine.get();
+
+		if (!engine) {
+			throw new Error("No template engine configured. Use framework().engine.get()");
 		}
 
-		return params;
-	}
+		try {
+			const rendered = await engine.render(view, data);
 
-	private extractQuery(url: string): Record<string, string> {
-		const parsedUrl = new URL(url);
-		const qs = new URL(url, parsedUrl.host).search;
-		const query = new URLSearchParams(qs);
-		const mapper: Record<string, string> = {};
-
-		for (const [key, value] of query.entries()) {
-			mapper[key] = value;
+			this.headers.set("Content-Type", "text/html");
+			this.body = rendered;
+		} catch (error: any) {
+			throw new Error(`Error rendering template: ${error.message}`);
 		}
 
-		return mapper;
+		return this;
 	}
 
-	private getCookie(name: string): string | undefined {
-		return this.cookiesInstance.get(name);
+	private setCookie(name: string, value: string, options?: CookiesOptions): this {
+		const cookie = this.cookiesInstance.set(name, value, options);
+
+		this.headersInstance.append("Set-Cookie", cookie);
+		return this;
 	}
 
-	private getAllCookies(): Record<string, string> {
-		return this.cookiesInstance.getAll();
+	private setCookiesInHeaders(): void {
+		const allCookies = this.cookiesInstance.getAll();
+
+		for (const [name, value] of Object.entries(allCookies)) {
+			this.headersInstance.append("Set-Cookie", this.cookiesInstance.set(name, value));
+		}
 	}
 }
 
-export type FetchRequest = Request;
-export { RequestWrapper as Request };
+export type FetchResponse = Response;
+export { ResponseWrapper as Response };
