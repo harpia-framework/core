7b4e062d5fb170d40351cb4b2981379efa7d282a
Lucas Nathan J. da Silva <lucasnathanj@gmail.com>
2024-12-28 15:26:47 -0300

    chore: export types

--- index.ts
+++ index.ts
@@ -4,6 +4,11 @@ import { Application } from "./src/server";
 export { Request } from "./src/request";
 export { Response } from "./src/response";
 
+export type { Application as Harpia } from "./src/server";
+export type { CorsOptions } from "./src/types/cors";
+export type { CookiesOptions } from "./src/types/cookies";
+export type { Store } from "./src/types/store";
+
 export default function harpia(): Application {
 	return Application.getInstance();
 }
